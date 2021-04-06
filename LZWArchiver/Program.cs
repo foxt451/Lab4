2e681a045a105824e2b53123153484689a4f1205
Sviatozar <svpetrenko123@gmail.com>
2021-04-06 16:02:22 +0300

    Add project

---
+++ LZWArchiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LZWArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

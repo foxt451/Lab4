d95327d32ca93b9ac15082608512a003e54abd1b
Anatolii <Parshykov>
2021-04-14 00:15:31 +0300

    FileWork

---
+++ LZWArchiver/FileWork.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+
+namespace LZWArchiver
+{
+    public class FileWork
+    {
+        public void WriteType(string path, string name)
+        {
+            using (StreamWriter sw = new StreamWriter(path+name, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(name);
+            }
+        }
+
+        public string ReadType(string path)
+        {
+            string filename = "";
+            using (StreamReader rd = new StreamReader(path))
+            {
+                rd.ReadLine();
+                filename = rd.ReadLine();
+            }
+            FileStream file = new FileStream(path + "/rating.csv", FileMode.OpenOrCreate);
+            File.WriteAllLines(file, 
+                File.ReadLines(file).Where(l => l != filename).ToList());
+
+            return filename;
+        }
+    }
+}
--- LZWArchiver/Program.cs
+++ LZWArchiver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using LZWArchiver;
 
 namespace LZWArchiver
 {
@@ -6,7 +8,34 @@ namespace LZWArchiver
     {
         static void Main(string[] args)
         {
-            new Encoder("test.bin", "out.bin").Encode();
+            string path = @"D:\Новая папка (2)\Lab4\LZWArchiver\bin\Debug\net5.0\";
+            string commands = Console.ReadLine();
+            string[] command = commands.Split(' ');
+            List<string> ListOfInputFile = new List<string>();//.txt and etc
+            if (command[0] == "--compress")
+            {
+                string NameFileOutput = command[1]; //.lzw
+                for (int i = 2; i < command.Length; i++)
+                {
+                    ListOfInputFile.Add(command[i]);
+                }
+                FileWork file = new FileWork();
+                for (int i = 0; i < ListOfInputFile.Count; i++)
+                {
+                    file.WriteType(path, ListOfInputFile[i]);
+                }
+                
+            }
+            else if (command[0] == "--decompress")
+            {
+                string NameInpFile = command[1]; // .lzw
+                FileWork file = new FileWork();
+                string NameOutpFile = file.ReadType(path + NameInpFile); // .exe and etc
+            }
+            else
+            {
+                throw new ArgumentException("Not indif operation");
+            }
         }
     }
 }
